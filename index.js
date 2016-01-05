2335c808b3d10d3ac33c2c9dd80a7348b03c024a
Amo Wu <amo260@gmail.com>
2016-01-05 12:38:27 +0800

    feat(aws): support AWS SNS
    Resolve Slack timeout problem

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+import axios from 'axios'
+import { get } from 'lodash'
+
 import config from './config.json'
 import messages from './messages.json'
 import google from './lib/google.js'
 
-type Event = {
+type SlackRequestObject = {
   token: string,
   team_id: string,
   team_domain: string,
@@ -11,29 +14,43 @@ type Event = {
   user_id: string,
   user_name: string,
   command: string,
-  text: string
+  text: string,
+  response_url: string
 }
 
 /**
- * Entrypoint for AWS Lambda
- * @param event is the JSON payload that API Gateway transformed from slack's application/x-www-form-urlencoded request
- * @param context has methods to let Lambda know when we're done - similar to http/express modules `response.send()`
+ * Entrypoint for AWS Lambda, this Lambda function receive Slack Slash Commands request from AWS SNS, and then send back a result response to Slack client.
+ * @param event is a AWS SNS Object that transformed from Slack's application/x-www-form-urlencoded request
+ * @param context has methods to let Lambda know when we're done, similar to express modules `response.send()`
  */
-export function handler (event: Event, context: any): void {
+export function handler (event: any, context: any): void {
   console.log(event)
 
-  // Verify request came from slack
-  if (event.token !== config.SLASH_COMMANDS_TOKEN) {
+  // Verify request came from SNS
+  const sns = get(event, ['Records', 0, 'Sns', 'Message'])
+  if (!sns) {
+    return context.fail(messages.SNS_MESSAGE_NOT_FOUND)
+  }
+
+  // Convert SNS message to JSON
+  const payload: SlackRequestObject = JSON.parse(sns)
+  console.log(payload)
+
+  // Verify request had a right Slack token
+  if (payload.token !== config.SLASH_COMMANDS_TOKEN) {
     return context.fail(messages.UNAUTHORIZED_TOKEN)
   }
 
-  google(event.text)
+  // Run command module and then send back a response to Slack client
+  google(payload.text)
     .then(response => {
-      context.succeed(response)
+      axios.post(payload.response_url, response)
+        .then(done => context.succeed(done))
+        .catch(error => context.fail(error))
     })
     .catch(error => {
       console.error(error)
-      context.succeed({
+      axios.post(payload.response_url, {
         'response_type': 'in_channel',
         'attachments': [
           {
@@ -42,6 +59,7 @@ export function handler (event: Event, context: any): void {
             'color': 'danger'
           }
         ]
-      })
+      }).then(done => context.succeed(done))
+        .catch(error => context.fail(error))
     })
 }
